d124fc765d7ee291755970f02a360d263d2b186f
prettyboy <francecar@me.com>
2025-02-14 17:14:46 +0100

    updated with word counter

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def main():
+def store_book():
     with open("books/frankestein.txt") as f:
         file_contents = f.read()
+    split_book = file_contents.split()
+    return split_book
+def count_words():
+    count = 0
+    stored_book = store_book()
+    for word in stored_book:
+        count += 1
+    return count
+
+def main():
+    print(count_words())
+
+
+
 main()
